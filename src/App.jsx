322c39fc1d32f6ec8e98b17b2e68364551fa0b7f
PutriAlmadeaa <almadeaputri20@gmail.com>
2024-06-05 13:45:10 +0700

    update Class Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+  
+  handleClick() {
+    this.setState({count: this.state.count + 1});
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={() => {this.handleClick()}}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
